449709eec76432aca19d1af7099616186f025843
Erkut Alakuş <erkutalakus@gmail.com>
2018-02-24 16:57:02 +0300

    JS Console => Python Out & Puzzle GUI

--- src/controller.py
+++ src/controller.py
@@ -1,4 +1,5 @@
 import htmlPy
+import datetime
 
 
 class Controller(htmlPy.Object):
@@ -11,6 +12,15 @@ class Controller(htmlPy.Object):
     def say_hello_world(self):
         self.app.html = u"Hello, world"
 
-    @htmlPy.Slot(str, result = str)
-    def get_puzzle(self, date):
+    @htmlPy.Slot(str, result=str)
+    def get_puzzle(self, json_data):
         return "testing"
+
+    @htmlPy.Slot(str, str)
+    def console_log(self, log, type):
+        if type == "log":
+            print(str(datetime.datetime.now().time()) + "[LOG] " + log)
+        elif type == "error":
+            print(str(datetime.datetime.now().time()) + "[ERROR] " + log)
+        else:
+            print(str(datetime.datetime.now().time()) + "[UNKNOWN] " + log)
